55807c7d7cfba1e43e987e063c48c5ec08ff9c18
Max Taylor-Davies <maxtaylordavies@gmail.com>
2019-12-07 00:07:09 +0000

    uncommented first half of prepare_data.sh

--- scripts/utils/prepare_data.sh
+++ scripts/utils/prepare_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#python3 make_hdf5.py --dataset SWET_ERYTHEMA --batch_size 256
+python3 make_hdf5.py --dataset SWET_ERYTHEMA --batch_size 256
 python3 calculate_inception_moments.py --dataset SWET_ERYTHEMA_hdf5
