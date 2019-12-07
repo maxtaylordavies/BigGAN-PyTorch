ff83831d6cb030c5c1633262123bab3082ee867a
Max L Taylor-Davies <mt4217@login-7.rcs.ic.ac.uk>
2019-12-07 03:05:42 +0000

    calculates inception moments

--- scripts/utils/prepare_data.sh
+++ scripts/utils/prepare_data.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
-python3 make_hdf5.py --dataset SWET_ERYTHEMA --batch_size 256
-python3 calculate_inception_moments.py --dataset SWET_ERYTHEMA_hdf5
+#PBS -l walltime=01:00:00,select=1:ncpus=1:mem=16gb:ngpus=1
+
+module load anaconda3/personal
+source activate biggan
+cd $PBS_O_WORKDIR
+
+#python make_hdf5.py --dataset SWET_ERYTHEMA --batch_size 256
+python calculate_inception_moments.py --dataset SWET_ERYTHEMA_hdf5
