cdd0a03ac3c4f99d979a1456d8c2a2a0631a2623
Max Taylor-Davies <maxtaylordavies@gmail.com>
2020-01-30 13:38:21 +0000

    modified pretraining script ready for rerun

--- scripts/utils/prepare_data.sh
+++ scripts/utils/prepare_data.sh
@@ -1,6 +1,7 @@
+PBS -l walltime=01:00:00,select=1:ncpus=1:mem=16gb:ngpus=1
 module load anaconda3/personal
 source activate biggan
 cd $PBS_O_WORKDIR
 
-#python make_hdf5.py --dataset SWET_ERYTHEMA --batch_size 256
+# python make_hdf5.py --dataset SWET_ERYTHEMA --batch_size 256
 python calculate_inception_moments.py --dataset SWET_ERYTHEMA_hdf5
